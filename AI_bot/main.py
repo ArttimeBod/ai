ecf0e61b5cd37b552d71baea06f78bc4040c7432
ArttimeBod <138983084+ArttimeBod@users.noreply.github.com>
2024-01-06 15:41:40 +0300

    Add files via upload

---
+++ AI_bot/main.py
@@ -0,0 +1,29 @@
+import discord
+from discord.ext import commands
+from model import get_class
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+TOKEN = 'MTEzMjI3NzgyMzEyNjYzNDQ5Nw.G-jEGy.OTbxrt6Z6LIN2gbvpPSYYw-4Viu2Q5N1U7A-yo'
+
+# команды
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+
+@bot.command()
+async def check(ctx):
+    if ctx.message.attachments:
+        for attachment in ctx.message.attachments:
+            file_name = attachment.filename
+            file_url = attachment.url
+            await attachment.save(f'./{attachment.filename}')
+            await ctx.send(get_class(image=f'./{attachment.filename}', model='keras_model.h5' , labels='labels.txt'))
+    else:
+        await ctx.send('Вы забыли прикрепит картинку :(')
+
+
+bot.run(TOKEN)
